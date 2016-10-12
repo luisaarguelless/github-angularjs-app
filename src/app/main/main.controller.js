98eaf61a1ff42c1f421d2183ac1ca1f027a16fea
luisarguelles <luisarguelles@seven4n.com>
2016-10-12 10:56:37 -0500

    Se agrega dependencia de ngCookies al bower.json. Se crea el html del formulario y se crea el modelo en el controlador y la funcionalidad que guarda la info del usuario en una cookie

--- src/app/main/main.controller.js
+++ src/app/main/main.controller.js
@@ -1,12 +1,54 @@
-(function() {
+(function () {
   'use strict';
 
   angular
     .module('githubAngularjsApp')
     .controller('MainController', MainController);
 
+  MainController.$inject = ['$cookies'];
   /** @ngInject */
-  function MainController() {
-    
+  function MainController($cookies) {
+    var vm = this;
+    vm.usuario = {};
+    vm.datepickerOptions = {
+      format: 'yyyy-mm-dd',
+      language: 'fr',
+      autoclose: true,
+      weekStart: 0
+    };
+    vm.opened = false;
+    vm.open = open;
+    vm.save = save;
+    vm.showInfoCookie = showInfoCookie;
+
+    function open() {
+      vm.opened = true;
+    }
+
+    vm.dateOptions = {
+      formatYear: 'yy',
+      startingDay: 1
+    };
+
+    function save() {
+      $cookies.put('info-usuario', angular.toJson(vm.usuario));
+    }
+
+    function setInfo() {
+      var usuario = $cookies.get('info-usuario');
+      if (angular.isDefined(usuario)) {
+        vm.usuario = angular.fromJson(usuario);
+        vm.usuario.fechaNacimiento = new Date(vm.usuario.fechaNacimiento);
+      }
+    }
+
+    function showInfoCookie(){
+      return $cookies.get('info-usuario');
+    }
+
+    function init() {
+      setInfo();
+    }
+    init();
   }
 })();
